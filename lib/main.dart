a950c07854f727f1eb20da489d1ce9808d8bb4cd
Meet006 <meetp0006@gmail.com>
2021-04-03 20:45:12 +0530

    topic 2 complate

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,10 +15,48 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("MeeT's Flutter Project"),
+        title: Text("MeeT's Project"),
       ),
-      body: Container(
-        child: Center(child: Text("HEllo MEEt")),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          color: Colors.teal,
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                    padding: const EdgeInsets.all(8),
+                    alignment: Alignment.center,
+                    width: 100,
+                    height: 100,
+                    color: Colors.red),
+                Container(
+                    padding: const EdgeInsets.all(8),
+                    alignment: Alignment.center,
+                    width: 100,
+                    height: 100,
+                    color: Colors.blue),
+                Container(
+                    padding: const EdgeInsets.all(8),
+                    alignment: Alignment.center,
+                    width: 100,
+                    height: 100,
+                    color: Colors.green),
+                Container(
+                    padding: const EdgeInsets.all(8),
+                    alignment: Alignment.center,
+                    width: 100,
+                    height: 100,
+                    color: Colors.yellow),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
